5855de01dac4837a904097bbfa56c0d717165ca0
Mahendran <mahendran@frslabs.com>
2023-03-26 03:44:43 +0530

    loaded the api response on table
    This code uses useEffect, useState hooks to fetch the data from an api and render the data in a table by using reactjs and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Alice', body: 'Austria',userId: 1},
+    {id: 2, title: 'Alice', body: 'Austria', userId: 2}
+  ]);
+  useEffect(() => {
+  //    axios('https://jsonplaceholder.typicode.com/posts').then(data =>{setPosts(data.data)})
+    fetch('https://jsonplaceholder.typicode.com/posts')
+     .then(data => data.json())
+     .then(data => {
+      setPosts(data);
+    } );
+  }, [])
+  console.log(posts);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">userId</th>
+        <th scope="col">title</th>
+        <th scope="col">body</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      
+        {posts.map(post => {
+        return (
+          <tr>
+            <th scope="row">{post.id}</th>
+            <td>{post.userId}</td>
+            <td>{post.title}</td>
+            <td>{post.body}</td>
+          </tr>
+        );
+        })}
+
+    </tbody>
+  </table>
   );
+  
 }
 
 export default App;
